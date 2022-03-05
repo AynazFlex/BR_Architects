ced3ee4c549f9d8e8a07a2c7d5601c215e21579f
AynazFlex <87998138+AynazFlex@users.noreply.github.com>
2022-03-06 01:53:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,15 @@
 
 const menuButton = document.body.querySelector('.menu-button');
 const menuPanel = document.body.querySelector('.nav-panel_right');
+const links = document.getElementById('nav-panel');
 
 
 menuButton.onclick = () => {
     menuButton.classList.toggle('close');
     menuPanel.classList.toggle('actice-nav');
+}
+
+links.onclick = () => {
+    menuButton.classList.remove('close');
+    menuPanel.classList.remove('actice-nav');
 }
